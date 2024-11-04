180eb90528931f319fffa5d833891593f3e9f863
qe170202 <Quocdinhkiu@gmail.com>
2024-11-04 16:51:29 +0700

    Final2DFarm

---
+++ Assets/Scripts/FireCamp/CampFireReaction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampFireReaction : CampFireHit
+{
+    public float time;
+    public GameObject fire;
+    GameObject toolbar;
+
+    private void Start()
+    {
+        
+    }
+
+    public override void Hit()
+    {
+        if (toolbar == null)
+        {
+            toolbar = GameObject.FindWithTag("toolbar");  // Tìm toolbar khi hộp thoại đã xuất hiện
+            if (toolbar == null)
+            {
+                Debug.LogError("Toolbar not found with tag 'toolbar' after dialog.");
+                return;
+            }
+        }
+
+        // Bật lửa và các thao tác với toolbar
+        fire.SetActive(true);
+        GameManager.instance.inventoryContainer.RemoveItem(GameManager.instance.toolbarControllerGlobal.GetItem, 5);
+        toolbar.SetActive(!toolbar.activeInHierarchy);
+        toolbar.SetActive(true);
+        StartCoroutine(LateCall());
+    }
+
+
+    IEnumerator LateCall()
+    {
+
+        yield return new WaitForSeconds(40);
+        fire.SetActive(false);
+    }
+}
